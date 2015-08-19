da25b3c68c072ef98fe5ef5d1f610e833f3ff3df
Sergey Vaysman <vaisman@bolid.ru>
2015-08-19 11:47:14 +0300

    Add a name and a role

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,7 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "John Doe";
+var formattedName = HTMLheaderName.replace("%data%", name);
+$("#header").append(formattedName);
+
+var role = "Web Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").append(formattedRole);
